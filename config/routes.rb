da02d9284e26d71bc51b7d9b3f7361e8e1f825c4
kevinricardojs <kevinricardojs@gmail.com>
2015-11-05 17:42:15 -0600

    fawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'inicio/index'
+
   resources :contribuyentes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'contribuyentes#index'
+   root 'inicio#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

d65c6fc5313409c0ff722928ae188a1727718ae2
Kevin Jimenez <kevinricardojs@gmail.com>
2015-11-13 16:35:38 -0600

    abrir en vantanas emergentes -_-!

--- app/assets/javascripts/mi.js
+++ app/assets/javascripts/mi.js
@@ -18,72 +18,21 @@ $(document).ready(function(){
 
 	var popover = $(objeto).children("div");
 	
-		$(popover).mouseleave(function() {
-			$(this).slideUp(400);
-			});
-
-
-	/*validar nit*/
-	function nitIsValid(nit) {
-    if (!nit) {
-        return true;
-    }
-
-    var nitRegExp = new RegExp('^[0-9]+(-?[0-9kK])?$');
-
-    if (!nitRegExp.test(nit)) {
-        return false;
-    }
-
-    nit = nit.replace(/-/, '');
-    var lastChar = nit.length - 1;
-    var number = nit.substring(0, lastChar);
-    var expectedCheker = nit.substring(lastChar, lastChar + 1).toLowerCase();
-
-    var factor = number.length + 1;
-    var total = 0;
-
-    for (var i = 0; i < number.length; i++) {
-        var character = number.substring(i, i + 1);
-        var digit = parseInt(character, 10);
-
-        total += (digit * factor);
-        factor = factor - 1;
-    }
-
-    var modulus = (11 - (total % 11)) % 11;
-    var computedChecker = (modulus == 10 ? "k" : modulus.toString());
-
-    return expectedCheker === computedChecker;
-}
-
-$('#contribuyente_nit').bind('change paste keyup', function (e) {
-    var $this = $(this);
-    var $parent = $this.parent();
-    var $prev = $this.prev();
-    var $guardar = $('.actions #guardar');
-    var nit = $this.val();
-
-    if (nit && nitIsValid(nit)) {
-        $parent.addClass('has-success');
-        $prev.addClass('fa-check');
-        $parent.removeClass('has-error');
-        $prev.removeClass('fa-exclamation-circle');
-        $guardar.removeClass('disabled');
-    } else if (nit) {
-        $parent.addClass('has-error');
-        $prev.addClass('fa-exclamation-circle');
-        $parent.removeClass('has-success');
-        $prev.removeClass('fa-check');
-        $guardar.addClass('disabled');
-    } else {
-        $parent.removeClass('has-error');
-        $prev.removeClass('fa-exclamation-circle');
-        $parent.removeClass('has-success');
-        $prev.removeClass('fa-check');
-        $guardar.addClass('disabled');
-
-    }
+  $(popover).mouseleave(function() {
+   $(this).slideUp(400);
 });
+
+  $('a').click(function  () {
+    if ($(this).attr("href") !== "#") 
+    {
+        if ($(this).attr("href") !== "/") 
+        {
+            window.open(this.href,'ventana','width=800,height=500'); return false;        
+        };
+        
+    }
+    
+
+})
 
 })
